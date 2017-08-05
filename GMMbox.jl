0c389b7401ae02cf79718d6d6936e43ae98d23ea
vidhyasaharan <vidhyasaharan@gmail.com>
2017-08-06 00:10:30 +1000

    added gmm type definition and kmeans constructor

--- GMMbox.jl
+++ GMMbox.jl
@@ -2,8 +2,9 @@ module GMMbox
 
 using Distances
 using Distributions
+using Clustering
 
-export gaussian, logprob!, logprob
+export gaussian, logprob!, logprob, gmm
 
 mutable struct gaussian{T<:AbstractFloat}
   ndim::Int64
@@ -11,8 +12,24 @@ mutable struct gaussian{T<:AbstractFloat}
   cov::Array{T,2}
 end
 
-gaussian(ndim::Integer) = gaussian(ndim::Integer,zeros(ndim),eye(ndim))
+gaussian(ndim::Integer) = gaussian(ndim,zeros(ndim,1),eye(ndim))
 gaussian(data::Matrix{T} where T<:AbstractFloat)  = gaussian(size(data,1),mean(data,2),cov(data,2))
+
+mutable struct gmm{T<:AbstractFloat}
+  nmix::Int64
+  wts::Array{T,2}
+  mix::Array{gaussian{T}}
+end
+
+function gmm(nmix::Integer, data::Matrix{T} where T <: AbstractFloat)
+  km = kmeans(data,nmix);
+  mix = Array{gaussian{eltype(data)}}(nmix);
+  for i=1:nmix
+    mix[i] = gaussian(data[:,find(km.assignments.==i)]);
+  end
+  return gmm(nmix,convert(Array{eltype(data)},(1/nmix)*ones(nmix,1)),mix);
+end
+
 
 
 logprob!(p::Array{T},g::gaussian{T},data::Matrix{T}) where T <: AbstractFloat = logpdf!(p,MvNormal(vec(g.mean),g.cov),data)
