68ac2ea274e3e9c08d3d0be0f28555f663974199
vidhyasaharan <vidhyasaharan@gmail.com>
2017-08-07 23:25:04 +1000

    scratchpad

--- test.jl
+++ test.jl
@@ -6,14 +6,11 @@ using Distributions
 #using Clustering
 
 
+#=
 ndata = convert(Array{Float32},rand(MvNormal(zeros(13),eye(13)),100));
-
 gm = gmm(4,ndata);
-
-
-
 data = rand(gm,500);
-
+=#
 
 
 fpath = "/home/vidhya/work/workspace/swb_mfcc";
@@ -30,24 +27,17 @@ ofile = "/home/vidhya/work/workspace/swb.mfc";
 data = read_binary(ofile);
 d1 = read_binary(flist[end]);
 
+
+
 data = convert(Matrix{Float32},data);
-
-
 g = gaussian(data);
 gm = gmm(4,data);
 
 
 t1 = zeros(typeof(data[1]),size(data,2));
-p = zeros(typeof(data[1]),size(data,2),1);
+p = zeros(typeof(data[1]),size(data,2));
 
-function logprob!(p::Array{T},gm::gmm{T},data::Matrix{T}) where T <: AbstractFloat
-  mixprob = zeros(eltype(data),size(data,2),gm.nmix);
-  for i=1:gm.nmix
-    @time mixprob[:,i] = logprob(gm.mix[i],data);
-    @time logprob!(t1,gm.mix[i],data);
-  end
-end
-
-
+@time p = logprob(gm,data);
+@time p = logprob(gm,data);
 @time logprob!(t1,g,data);
 @time logprob!(t1,g,data);
