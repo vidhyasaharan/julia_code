8d5d45ca8b1b59d23a6d39673b6dfd04db027ff3
vidhyasaharan <vidhyasaharan@gmail.com>
2017-08-10 14:35:01 +1000

    scratchpad

--- test.jl
+++ test.jl
@@ -6,27 +6,26 @@ using Distributions
 #using Clustering
 
 
-#=
+
 ndata = convert(Array{Float32},rand(MvNormal(zeros(13),eye(13)),100));
 gm = gmm(4,ndata);
-data = rand(gm,500);
-=#
+data = rand(gm,5000000);
 
 
+#=
 fpath = "/home/vidhya/work/workspace/swb_mfcc";
 mfc_flist = filter(x->endswith(x,".mfc"),readdir(fpath));
 flist = mfc_flist;
 for i=1:length(mfc_flist)
   flist[i] = joinpath(fpath,mfc_flist[i]);
 end
+=#
 
+#=
 ofile = "/home/vidhya/work/workspace/swb.mfc";
-
 #combine_binary(ofile,flist);
-
 data = read_binary(ofile);
-d1 = read_binary(flist[end]);
-
+=#
 
 
 data = convert(Matrix{Float32},data);
@@ -34,10 +33,14 @@ g = gaussian(data);
 gm = gmm(4,data);
 
 
-t1 = zeros(typeof(data[1]),size(data,2));
-p = zeros(typeof(data[1]),size(data,2));
+p = zeros(eltype(data),size(data,2));
 
+#=
 @time p = logprob(gm,data);
 @time p = logprob(gm,data);
-@time logprob!(t1,g,data);
-@time logprob!(t1,g,data);
+@time logprob!(p,gm,data);
+=#
+
+@time @synch for i=1:10
+  logprob!(p,gm,data);
+end
