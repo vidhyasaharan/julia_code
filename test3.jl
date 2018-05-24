1f12e788a84001163a8ad5f86531639e9c2d7d85
vidhyasaharan <vidhyasaharan@gmail.com>
2018-05-25 09:41:08 +1000

    playing with GP visualisation by sampling Gaussians

--- test3.jl
+++ test3.jl
@@ -1,6 +1,27 @@
 using Distributions
 using Gadfly
 
-x = rand(MultivariateNormal([0.0,0.0],[1.0 0.5; 0.5 1.0]),10000);
+ndim = 10;
+mn = zeros(ndim);
+cv = zeros(ndim,ndim)
 
-plot(x=x[1,:],y=x[2,:],Geom.hexbin(xbincount=300,ybincount=300))
+sq_exp_kernel(x::Number,y::Number,len::Number) = exp(-(x-y)^2/len)
+
+len = 3;
+for i=1:ndim
+    for j=1:i
+        cv[i,j] = sq_exp_kernel(i,j,len);
+        cv[j,i] = cv[i,j];
+    end
+end
+
+mvn = MultivariateNormal(mn,cv);
+
+nsam = 20;
+x = rand(mvn,nsam);
+for i=1:nsam
+    display(plot(x=1:ndim,y=x[:,i],Geom.line,Coord.cartesian(ymin=-4, ymax=4)))
+    sleep(.5)
+end
+
+x_fixed = 
