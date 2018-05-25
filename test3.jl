3f68c187211e2fae0c8bff73d4aad592da0c063b
vidhyasaharan <vidhyasaharan@gmail.com>
2018-05-26 01:05:05 +1000

    Simple GP regression

--- test3.jl
+++ test3.jl
@@ -1,25 +1,31 @@
 using Distributions
 using Gadfly
 
-ndim = 10;
-mn = zeros(ndim);
-cv = zeros(ndim,ndim)
 
-sq_exp_kernel(x::Number,y::Number,len::Number) = exp(-(x-y)^2/len)
+se_kernel(p::Number,q::Number,len::Number) = exp(-((p-q)/(2*len))^2);
 
-len = 3;
-for i=1:ndim
-    for j=1:i
-        cv[i,j] = sq_exp_kernel(i,j,len);
-        cv[j,i] = cv[i,j];
+function gen_se_cov(x,len::Number)
+    nsam = length(x)
+    cv = zeros(nsam,nsam);
+    for i=1:nsam
+        for j=1:i
+            cv[i,j] = se_kernel(x[i],x[j],len);
+            cv[j,i] = cv[i,j];
+        end
     end
+    return cv
 end
+
+x = linspace(0,1,15);
+len = .2;
+cv = gen_se_cov(x,len);
+mn = zeros(length(x));
 
 mvn = MultivariateNormal(mn,cv);
 
 nsam = 20;
-x = rand(mvn,nsam);
+f = rand(mvn,nsam);
 for i=1:nsam
-    display(plot(x=1:ndim,y=x[:,i],Geom.line,Coord.cartesian(ymin=-4, ymax=4)))
+    display(plot(x=x,y=f[:,i],Geom.line,Coord.cartesian(ymin=-4, ymax=4)))
     sleep(.5)
 end
