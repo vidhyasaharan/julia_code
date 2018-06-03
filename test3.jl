70ea5c44abfb56af53db60baff593c6cea05221e
vidhyasaharan <vidhyasaharan@gmail.com>
2018-06-03 23:18:31 +1000

    Single function for generating covariance matrics

--- test3.jl
+++ test3.jl
@@ -2,9 +2,55 @@ using Distributions
 using Gadfly
 
 
-se_kernel(p::Number,q::Number,len::Number) = exp(-((p-q)/(2*len))^2);
+# struct se_knl
+#     len::AbstractFloat
+# end
 
-function gen_se_cov(x,len::Number)
+# function (knl::se_knl)(p::T,q::T) where T<:AbstractFloat
+#     return exp(-((p-q)/(2*knl.len))^2)
+# end
+
+# function gen_cov(x,kernel::se_knl)
+#     nsam = length(x);
+#     cv = zeros(nsam,nsam);
+#     for i=1:nsam
+#         for j=1:i
+#             cv[i,j] = kernel(x[i],x[j]);
+#             cv[j,i] = cv[i,j];
+#         end
+#     end
+#     return cv
+# end
+
+
+struct kernel_param
+    se_len::AbstractFloat
+end
+
+function kernel_param(;se_len=1)
+    kernel_param(se_len)
+end
+
+
+se_krnl(p::T,q::T,param::kernel_param) where T<:AbstractFloat = exp(-((p-q)/(2*param.se_len))^2);
+
+function gen_cv(x,kernel::Function,param::kernel_param)
+    nsam = length(x);
+    cv = zeros(nsam,nsam);
+    for i=1:nsam
+        for j=1:i
+            cv[i,j] = kernel(x[i],x[j],param);
+            cv[j,i] = cv[i,j];
+        end
+    end
+    return cv
+end
+
+
+
+se_kernel(p::T,q::T,len::T) where T<:AbstractFloat = exp(-((p-q)/(2*len))^2);
+
+function gen_se_cov(x::AbstractArray{T},len::T) where T<:AbstractFloat
     nsam = length(x)
     cv = zeros(nsam,nsam);
     for i=1:nsam
@@ -15,6 +61,37 @@ function gen_se_cov(x,len::Number)
     end
     return cv
 end
+
+
+function gen_covs(x::AbstractArray{T},y::AbstractArray{T},kernel_type = "squared error",param...) where T<:AbstractFloat
+    if kernel_type == "squared error"
+        kernel = se_kernel;
+        knl_param = param[1];
+    end
+
+    if(x==y)
+        nsam = length(x)
+        covs = zeros(nsam,nsam);
+        for i=1:nsam
+            for j=1:i
+                covs[i,j] = kernel(x[i],x[j],knl_param);
+                covs[j,i] = covs[i,j];
+            end
+        end
+    else
+        nx = length(x);
+        ny = length(y);
+        covs = zeros(nx,ny);
+        for i=1:nx
+            for j=1:ny
+                covs[i,j] = kernel(x[i],y[j],knl_param);
+            end
+        end
+    end
+    return covs
+end
+
+
 
 function gen_se_xcov(x,y,len::Number)
     nx = length(x);
@@ -46,7 +123,7 @@ function plotmvnSamplesTrainPoints(nsam,dist,trainx,trainf)
 end
 
 
-x = linspace(-5,5,50);
+x = linspace(-5,5,5000);
 len = .3;
 cv = gen_se_cov(x,len);
 mn = zeros(length(x));
