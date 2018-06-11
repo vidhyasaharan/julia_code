bc13bbc8ee8655655e0b3be812ca5d4dacda46b5
vidhyasaharan <vidhyasaharan@gmail.com>
2018-06-11 15:23:23 +1000

    Updated covariance generation functions for vector data

--- GPbox.jl
+++ GPbox.jl
@@ -1,7 +1,10 @@
 module GPbox
 
+const Float = Float64
+
 using Gadfly: layer, Geom
 using Distributions
+using Distances
 
 export PlotGPsamples1D, estGP, GPkernel, genCovs
 
@@ -11,49 +14,32 @@ struct GPkernel
     param::Dict
 end
 
-#Squared error kernel for scalars
 
-se_kernel(p::T,q::T,len::T) where T<:AbstractFloat = exp((-((p-q)/(len))^2)/2);
-
-function genCovs(x::AbstractArray{T},y::AbstractArray{T},gpk::GPkernel) where T<:AbstractFloat
-    if gpk.kernel_type == "squared error"
-        kernel::Function = se_kernel;
-        knl_param::T = gpk.param["len"];
-    end
-
-    if(x==y)
-        nsam = length(x)
-        covs = zeros(nsam,nsam);
-        for i=1:nsam
-            for j=1:i
-                covs[i,j] = kernel(x[i],x[j],knl_param);
-                covs[j,i] = covs[i,j];
-            end
-        end
-    else
-        nx = length(x);
-        ny = length(y);
-        covs = zeros(nx,ny);
-        for i=1:nx
-            for j=1:ny
-                covs[i,j] = kernel(x[i],y[j],knl_param);
-            end
-        end
+####Covariance Matrix Generation#### 
+function genCovs(x::Matrix{T},y::Matrix{T},gpk::GPkernel) where T<:Float
+    if(gpk.kernel_type=="squared exp")
+        nx = size(x,2);
+        ny = size(y,2);
+        lensq::T = (get(gpk.param,"len",1))^2;
+        len_fact::T = 1/(2*lensq);
+        covs = pairwise(SqEuclidean(),x,y);
+        covs = exp.((-covs).*len_fact);
     end
     return covs
 end
 
 
 
-
 ####Estimating posterior GP (mean and covarainces) given training point, test points and GP kernel####
-function estGP(test_pts::AbstractArray{T},train_pts::AbstractArray{T},train_vals::AbstractArray{T},gpk::GPkernel) where T<:AbstractFloat
+function estGP(test_pts::AbstractArray{T},train_pts::AbstractArray{T},train_vals::AbstractArray{T},gpk::GPkernel) where T<:Float
     cv = genCovs(test_pts,test_pts,gpk);
-    mn = zeros(length(test_pts));
+    num_testpts = size(test_pts,2);
+    num_fdim = size(train_vals,1);
+    mn = zeros(num_fdim,num_testpts);
     traincv = genCovs(train_pts,train_pts,gpk);
     crosscv = genCovs(test_pts,train_pts,gpk);
     itraincv = inv(traincv);
-    gp_mn = crosscv*itraincv*train_vals;
+    gp_mn = crosscv*itraincv*train_vals';
     temp = crosscv*itraincv*crosscv';
     gp_cv = cv - (temp + temp')/2;  #hack for positive definite CV
     return gp_mn, gp_cv
@@ -65,7 +51,7 @@ end
 
 #Draw samples from 1D GP and output as array of Gadfly plotting layers
 function PlotGPsamples1D(test_pts,nsam,gp_mn,gp_cov)
-    dist = MultivariateNormal(gp_mn,gp_cov)
+    dist = MultivariateNormal(gp_mn[:],gp_cov)
     f = rand(dist,nsam);
     layers = [layer(
                     x=test_pts,
